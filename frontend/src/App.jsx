fc87349e58ac08a685a1fb8ac5c6f5525154929f
ParthMinddeft <parth.talsaniya@minddeft.net>
2024-03-12 17:59:20 +0530

    add the home page and design

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,28 +5,44 @@ import Login from "./pages/login/Login";
 import Signup from "./pages/signup/Signup";
 import { Toaster } from "react-hot-toast";
 import { useAuthContext } from "./context/AuthContext";
+import "./app.scss";
+import Navbar from "./components/navbar/Navbar";
+import Hero from "./components/hero/Hero";
+import Parallax from "./components/parallax/Parallax";
 
-function App() {
+const App = () => {
   const { authUser } = useAuthContext();
   return (
-    <div className="p-4 h-screen flex items-center justify-center">
-      <Routes>
+    <div>
+      <section className="text-white" id="HomePage">
+        <Navbar />
+        <Hero />
+      </section>
+      <section className="text-white" id="Services">
+        <Parallax type="services" />
+      </section>
+      <section className="text-white" id="Contact"></section>
+      <section className="text-white" id="About"></section>
+      {/* // <div className="p-4 h-screen flex items-center justify-center"> */}
+      {/* <Routes>
         <Route
-          path="/"
-          element={authUser ? <Home /> : <Navigate to={"/login"} />}
+        path="/"
+        element={authUser ? <Home /> : <Navigate to={"/login"} />}
         />
         <Route
-          path="/login"
-          element={authUser ? <Navigate to={"/"} /> : <Login />}
+        path="/login"
+        element={authUser ? <Navigate to={"/"} /> : <Login />}
         />
         <Route
-          path="/signup"
-          element={authUser ? <Navigate to={"/"} /> : <Signup />}
+        path="/signup"
+        element={authUser ? <Navigate to={"/"} /> : <Signup />}
         />
-      </Routes>
+        </div>
+      </Routes> */}
       <Toaster />
+      //{" "}
     </div>
   );
-}
+};
 
 export default App;
---
+++ frontend/src/components/sideMenu/SideMenu.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+import "./sidemenu.scss";
+import ToggleButton from "./toggleButton/ToggleButton";
+import Links from "./links/Links";
+import { motion } from "framer-motion";
+
+const variants = {
+  open: {
+    clipPath: "circle(1200px at 50px 50px)",
+    transition: {
+      type: "spring",
+      stiffness: 20,
+    },
+  },
+  closed: {
+    clipPath: "circle(30px at 50px 50px)",
+    transition: {
+      delay: 0.5,
+      type: "spring",
+      stiffness: 400,
+      damping: 40,
+    },
+  },
+};
+const SideMenu = () => {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <motion.div className="sidemenu" animate={open ? "open" : "closed"}>
+      <motion.div className="bg" variants={variants}>
+        <Links />
+      </motion.div>
+      <ToggleButton setOpen={setOpen} />
+    </motion.div>
+  );
+};
+
+export default SideMenu;
