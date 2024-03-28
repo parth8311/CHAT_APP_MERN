bf3bb244ffa0c15000069fd7b920da58f10478cb
ParthMinddeft <parth.talsaniya@minddeft.net>
2024-03-28 12:48:36 +0530

    remove buttons

--- frontend/src/components/sideMenu/links/Links.jsx
+++ frontend/src/components/sideMenu/links/Links.jsx
@@ -27,7 +27,7 @@ const itemVariants = {
 };
 
 const Links = () => {
-  const items = ["HomePage", "Services", "Portfolio", "Contact", "About"];
+  const items = ["HomePage", "Services", "Contact"];
 
   return (
     <motion.div className="links" variants={variants}>
